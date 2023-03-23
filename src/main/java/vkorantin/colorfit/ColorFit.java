56dae82a8589d45642716d8cb7f6c2b25918342c
Korantin Varniere <korantin.varniere.etu@univ-lille.fr>
2023-03-23 20:28:51 +0100

    First scene

--- src/main/java/vkorantin/colorfit/ColorFit.java
+++ src/main/java/vkorantin/colorfit/ColorFit.java
@@ -1,9 +1,28 @@
 package vkorantin.colorfit;
 
-public class ColorFit {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class ColorFit extends Application {
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("ColorFit");
+		
+		StackPane root = new StackPane();
+		Label helloLabel = new Label("Hello World !");
+		root.getChildren().add(helloLabel);
+		
+		stage.setScene(new Scene(root, 1280, 720));
+		
+		stage.show();
 	}
 	
 }
