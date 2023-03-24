ee4b79831b14af6ebc26c8246adb132f5dc4700a
Korantin Varniere <korantin.varniere.etu@univ-lille.fr>
2023-03-24 11:42:15 +0100

    Can choose a color manually

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module vkorantin.colorfit {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.base;
 
     opens vkorantin.colorfit to javafx.fxml;
     exports vkorantin.colorfit;
--- src/main/java/vkorantin/colorfit/graphics/Graphics.java
+++ src/main/java/vkorantin/colorfit/graphics/Graphics.java
@@ -2,14 +2,22 @@ package vkorantin.colorfit.graphics;
 
 import java.util.List;
 
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import vkorantin.colorfit.controllers.UpdateController;
 import vkorantin.colorfit.model.Model;
 
@@ -31,11 +39,11 @@ public abstract class Graphics {
 		backButton = (Button) root.lookup("#back_btn");
 		exportButton = (Button) root.lookup("#export_btn");
 		
-		colorSlider.valueProperty().addListener(UpdateController.ColorSliderListener);
 		initialSetup(root);
 	}
 	
 	private static void initialSetup(Parent root) {
+		colorSlider.valueProperty().addListener(UpdateController.ColorSliderListener);
 		updateColors();
 		importButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
 			Model._import();
@@ -100,6 +108,39 @@ public abstract class Graphics {
 			colorsBox.getChildren().add(new Rectangle(100, 100, color));
 			codesBox.getChildren().add(new Label(String.format("(%d, %d, %d)", (int) (color.getRed()*255), (int) (color.getGreen()*255), (int) (color.getBlue()*255))));
 			greysBox.getChildren().add(new Rectangle(100, 100, Model.generateGreyFrom(color)));
+		}
+		
+		addColorChoosers();
+	}
+	
+	private static void addColorChoosers() {
+		for (Node node : colorsBox.getChildren()) {
+			node.addEventHandler(MouseEvent.MOUSE_ENTERED, (event) -> {
+				node.setCursor(Cursor.HAND);
+			});
+			node.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> {
+				ColorPicker picker = new ColorPicker((Color) ((Rectangle) node).getFill());
+				Button save = new Button("Save");
+				
+				save.addEventHandler(MouseEvent.MOUSE_CLICKED, (event2) -> {
+					((Rectangle) node).setFill(picker.getValue());
+					sortColors();
+				});
+
+				Stage stage = new Stage();
+				FlowPane root = new FlowPane();
+		        root.setPadding(new Insets(10));
+		        root.setHgap(10);
+		        root.getChildren().addAll(picker, save);
+		        
+		        Scene scene = new Scene(root, 300, 200);
+		        
+		        stage.setTitle("ColorPicker");
+		 
+		        stage.setScene(scene);
+		        stage.show();
+			});
+			Tooltip.install(node, new Tooltip("Left click to choose another color."));
 		}
 	}
 	
